ac44b79034c5dc268a6b279d170ca5fdd70a4e13
Daniel Manley <dmen1002@msn.com>
2021-06-11 11:35:13 -0500

    updated web manifest and service worker

--- public/index.js
+++ public/index.js
@@ -1,18 +1,7 @@
 let transactions = [];
 let myChart;
-
-fetch("/api/transaction")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    // save db data on global variable
-    transactions = data;
-
-    populateTotal();
-    populateTable();
-    populateChart();
-  });
+let db;
+const request = window.indexedDB.open("budget", 1);
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
@@ -66,14 +55,14 @@ function populateChart() {
 
   myChart = new Chart(ctx, {
     type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
+    data: {
+      labels,
+      datasets: [{
+        label: "Total Over Time",
+        fill: true,
+        backgroundColor: "#6666ff",
+        data
+      }]
     }
   });
 }
@@ -111,7 +100,7 @@ function sendTransaction(isAdding) {
   populateChart();
   populateTable();
   populateTotal();
-  
+
   // also send to server
   fetch("/api/transaction", {
     method: "POST",
@@ -121,33 +110,65 @@ function sendTransaction(isAdding) {
       "Content-Type": "application/json"
     }
   })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
       // clear form
       nameEl.value = "";
       amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
-
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+    });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").onclick = function () {
   sendTransaction(false);
 };
+
+
+function populateDb() {
+  fetch("/api/transaction")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // save db data on global variable
+      transactions = data;
+ 
+      populateTotal();
+      populateTable();
+      populateChart();
+
+      var budgetConnection = db.transaction(["budget"], "readwrite");
+      data.forEach((record) => {
+        var objectStore = transaction.objectStore("budget");
+        var objectStoreRequest = objectStore.add(record);
+      })
+      objectStoreRequest.onsuccess = function(event) {
+      };
+    });
+};
+
+request.onsuccess = function (event) {
+  // store the result of opening the database in the db variable.
+  // This is used a lot below
+  db = request.result;
+};
+
+populateDb();
